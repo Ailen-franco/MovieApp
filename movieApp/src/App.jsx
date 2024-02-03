7e297a21ad2dd4932fbc5733457511fa4e2213aa
Ailen Franco <ailenyamilaf@hotmail.com>
2024-02-03 02:18:55 -0300

    add routes at app.jsx

--- movieApp/src/App.jsx
+++ movieApp/src/App.jsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import FavoritesMovies from './components/FavoritesMovies';
+import { Header } from './components/Header';
+import Home from './components/Home';
+import LatestReleasesMovies from './components/LatestReleasesMovies';
+import PopularMovies from './components/PopularMovies';
+import SearchMovie from './components/SearchMovie';
+import DetailMovie from './components/DetailMovie'
+import Footer from './components/Footer';
+import { FavoritosContextProvider } from './context/favoritesContext';
 
 function App() {
-  
-
   return (
-    <>
-      
-    </>
-  )
+    <FavoritosContextProvider>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/latestReleasesMovies" element={<LatestReleasesMovies />} />
+          <Route path="/popularMovies" element={<PopularMovies />} />
+          <Route path="/favoritesMovies" element={<FavoritesMovies />} />
+          <Route path="/searchMovie" element={<SearchMovie />} />
+          <Route path='/detail/:movieId' element={<DetailMovie />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </FavoritosContextProvider>
+  );
 }
 
-export default App
+export default App;
